cac0199a6470143cbde677a1659a7e5c3c1b1cad
LAPTOP-I43HEEAB\windows-10 <arkhabagdana1@gmail.com>
2023-06-07 20:16:56 +0700

    modified login,register page and modified app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,redirect,url_for,render_template,request, url_for, jsonify
 from pymongo import MongoClient
-import jwt
+import jwt as pyjwt
 from datetime import datetime, timedelta
 import hashlib
 import requests
@@ -13,6 +13,8 @@ app=Flask(__name__)
 dotenv_path = join(dirname(__file__),'.env')
 load_dotenv(dotenv_path)
 
+SECRET_KEY = 'NOURISH'
+app.secret_key = 'NOURISH'
 MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME = os.environ.get("DB_NAME")
 
@@ -27,6 +29,34 @@ def home():
 @app.route('/admin',methods=['GET'])
 def admin():
     return render_template('admin.html')
+
+@app.route('/register_save', methods=['POST'])
+def register_save():
+    username_receive = request.form["username_give"]
+    namaUsaha_receive = request.form["namaUsaha_give"]
+    password_receive = request.form["password_give"]
+    sosialMedia_receive = request.form["sosialMedia_give"]
+    kategori_receive = request.form["kategori_give"]
+    daerah_receive = request.form["daerah_give"]
+    deskripsiUsaha_receive = request.form["deskripsiUsaha_give"]
+    password_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
+
+    doc = {
+        "username" : username_receive,
+        "nama_usaha" : namaUsaha_receive,
+        "password" : password_hash,
+        "sosial_media" : sosialMedia_receive,
+        "kategori" : kategori_receive,
+        "daerah" : daerah_receive,
+        "deskripsi_usaha" : deskripsiUsaha_receive,
+        "status" : "nonactive"
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result':'success'})
+
+@app.route('/register',methods=['GET'])
+def register():
+    return render_template("register.html")
 
 @app.route('/tentang_nourish',methods=['GET'])
 def tentang_nourish():
@@ -43,19 +73,18 @@ def diskusi():
    
     return render_template('diskusi.html')
 
-@app.route('/register',methods=['GET'])
-def register():
-    return render_template("register.html")
-
 @app.route('/login',methods=['GET'])
 def login():
-   
     return render_template('login.html')
 
 @app.route('/profil/<username>',methods=['GET'])
 def user(username):
    
     return render_template('profil.html')
+
+@app.route('/umkm_page',methods = ['GET'])
+def umkm_page():
+     return render_template('umkm-page.html')
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
